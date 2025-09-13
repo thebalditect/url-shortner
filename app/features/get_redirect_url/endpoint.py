62c24fc93b8e6c15fae6a61ba0d236492075b7eb
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-09-14 01:48:33 +0530

    fix: format files

--- app/features/get_redirect_url/endpoint.py
+++ app/features/get_redirect_url/endpoint.py
@@ -3,6 +3,7 @@ from fastapi.responses import RedirectResponse
 
 router = APIRouter()
 
-@router.get("/{short_code}", status_code= 302)
+
+@router.get("/{short_code}", status_code=302)
 async def get_redirect_url(short_code):
     return RedirectResponse("https://google.com")
--- app/features/shorten_url/endpoint.py
+++ app/features/shorten_url/endpoint.py
@@ -2,12 +2,15 @@ from pydantic import BaseModel, HttpUrl
 from fastapi import APIRouter
 import secrets
 
+
 class ShortenUrlRequest(BaseModel):
     original_url: HttpUrl
+
 
 class ShortenUrlResponse(BaseModel):
     short_code: str
     shortened_url: HttpUrl
+
 
 async def shorten(original_url: str):
     short_code = secrets.token_urlsafe(6)
@@ -17,8 +20,7 @@ async def shorten(original_url: str):
 
 router = APIRouter()
 
-@router.post("/urls/shorten", response_model= ShortenUrlResponse, status_code= 201)
+
+@router.post("/urls/shorten", response_model=ShortenUrlResponse, status_code=201)
 async def shorten_url(request: ShortenUrlRequest):
     return await shorten(request.original_url)
-
-
